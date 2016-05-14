346336578318df06fe2f9b2fdb12c6197bbc15ca
Toke Jepsen <tokejepsen@gmail.com>
2016-05-14 14:21:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+from distutils.core import setup
 
+setup(
+    name='pyblish-bumpybx',
+    version='0.1.0',
+    packages=['pyblish_bumpybox',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
