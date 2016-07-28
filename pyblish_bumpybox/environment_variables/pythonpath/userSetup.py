b301a663b59155d88a66fb2f0bb86a73dd777bd3
Toke Jepsen <toke.jepsen@baitstudio.local>
2016-07-28 17:02:27 +0100

    maya: wip work on sets workflow

--- pyblish_bumpybox/environment_variables/pythonpath/userSetup.py
+++ pyblish_bumpybox/environment_variables/pythonpath/userSetup.py
@@ -15,6 +15,18 @@ def toggle_instance(instance, new_value, old_value):
     if instance.data['family'] == 'alembic.asset':
         pymel.core.PyNode(instance).pyblish_alembic.set(bool(new_value))
 
+    if "families" in instance.data and "cache.*" in instance.data["families"]:
+
+        attr = instance.data["family"].replace(".", "_")
+        node = instance.data["set"]
+
+        if pymel.core.attributeQuery(attr, node=node.name(), exists=True):
+            node.attr(attr).set(new_value)
+        else:
+            pymel.core.addAttr(node, longName=attr, defaultValue=new_value,
+                               attributeType='bool')
+
+
 pyblish.api.register_callback("instanceToggled", toggle_instance)
 
 
