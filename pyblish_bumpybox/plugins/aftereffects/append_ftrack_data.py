9b9a918cd3bed6d47adb135ed49ad14d441b0516
Toke Jepsen <tokejepsen@gmail.com>
2016-10-12 22:17:32 +0100

    adding audio to after effects submissions

---
+++ pyblish_bumpybox/plugins/aftereffects/append_ftrack_data.py
@@ -0,0 +1,20 @@
+import pyblish.api
+import ftrack
+
+
+class AppendFtrackAudio(pyblish.api.ContextPlugin):
+
+    label = "Ftrack Audio"
+    order = pyblish.api.ExtractorOrder
+
+    def process(self, context):
+
+        # get audio file
+        task = ftrack.Task(context.data["ftrackData"]["Task"]["id"])
+        try:
+            asset = task.getParent().getAsset("audio", "audio")
+            component = asset.getVersions()[-1].getComponent()
+            audio_file = component.getFilesystemPath()
+            context.data["audio"] = audio_file
+        except:
+            self.log.warning("Couldn't find any audio file on Ftrack.")
