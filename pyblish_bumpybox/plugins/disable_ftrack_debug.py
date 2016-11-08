1bc2353b16b28c94343841cd976ef14490390e1e
Toke Jepsen <tokejepsen@gmail.com>
2016-11-08 16:26:37 +0100

    disable ftrack debug messages

---
+++ pyblish_bumpybox/plugins/disable_ftrack_debug.py
@@ -0,0 +1,15 @@
+import logging
+
+import pyblish.api
+
+
+class DisableFtrackDebugMessages(pyblish.api.ContextPlugin):
+    """ Disable the debug messages from the ftrack api. """
+
+    order = pyblish.api.CollectorOrder
+
+    def process(self, context):
+
+        for log in logging.Logger.manager.loggerDict.keys():
+            if "ftrack" in log:
+                logging.getLogger(log).setLevel(logging.WARNING)
