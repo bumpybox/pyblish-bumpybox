ab2359e57b13695419bf7e041b6b40bc70571957
Toke Jepsen <tokejepsen@gmail.com>
2016-11-15 10:51:42 +0100

    Simplify ftrack data plugins

--- pyblish_bumpybox/plugins/append_ftrack_data.py
+++ pyblish_bumpybox/plugins/append_ftrack_data.py
@@ -1,22 +1,47 @@
 import pyblish.api
 
 
-class AppendFtrackData(pyblish.api.InstancePlugin):
-    """ Appending ftrack component and asset type data """
+class AppendFtrackAssetData(pyblish.api.InstancePlugin):
+    """ Appending ftrack asset type and asset name data """
 
-    families = ["img.*", "cache.*"]
-    # order before default ftrack plugins
-    order = pyblish.api.ValidatorOrder - 0.4
+    order = pyblish.api.ValidatorOrder - 0.49
 
     def process(self, instance):
 
-        # ftrack data
-        if not instance.context.has_data("ftrackData"):
+        # assign components to activate processing
+        components = instance.data.get("ftrackComponents", {})
+        instance.data["ftrackComponents"] = components
+
+        # assigning asset type
+        if "families" in instance.data:
+            if "img.*" in instance.data["families"]:
+                instance.data["ftrackAssetType"] = "img"
+            if "cache.*" in instance.data["families"]:
+                instance.data["ftrackAssetType"] = "cache"
+
+        if instance.data["family"] == "scene":
+            instance.data["ftrackAssetType"] = "scene"
+
+        # skipping if not launched from ftrack
+        if "ftrackData" not in instance.context.data:
             return
 
-        instance.data["ftrackComponents"] = {}
+        # assigning asset name
+        ftrack_data = instance.context.data["ftrackData"]
+        asset_name = ftrack_data["Task"]["name"]
+        instance.data["ftrackAssetName"] = asset_name
 
-        if "img.*" in instance.data["families"]:
-            instance.data["ftrackAssetType"] = "img"
-        if "cache.*" in instance.data["families"]:
-            instance.data["ftrackAssetType"] = "cache"
+
+class AppendFtrackComponents(pyblish.api.InstancePlugin):
+    """ Appending output files from local extraction as components. """
+
+    order = pyblish.api.ExtractorOrder + 0.49
+
+    def process(self, instance):
+
+        if "outputPath" in instance.data:
+            components = instance.data.get("ftrackComponents", {})
+            data = {"path": instance.data["outputPath"],
+                    "overwrite": True}
+            components[str(instance)] = data
+            instance.data["ftrackComponents"] = components
