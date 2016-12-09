42fc0798a52882ffe3dc076aa168204e2c0a4640
Toke Jepsen <tokejepsen@gmail.com>
2016-12-09 15:29:33 +0100

    Remove ftrack related data from the standard scene collector.

--- pyblish_bumpybox/plugins/collect_scene.py
+++ pyblish_bumpybox/plugins/collect_scene.py
@@ -17,20 +17,7 @@ class BumpyboxCollectScene(pyblish.api.ContextPlugin):
         # create instance
         instance = context.create_instance(name=os.path.basename(current_file))
 
-        instance.data["family"] = "scene"
+        instance.data["families"] = ["scene"]
         instance.data["path"] = current_file
         label = "{0} - scene".format(os.path.basename(current_file))
         instance.data["label"] = label
-
-        # ftrack data
-        if "ftrackData" not in instance.context.data:
-            return
-
-        instance.data["ftrackAssetType"] = "scene"
-
-        ftrack_data = instance.context.data("ftrackData")
-        instance.data["ftrackAssetName"] = ftrack_data["Task"]["name"]
-
-        component_name = "%s_work" % pyblish.api.current_host()
-        components = {component_name: {"path": current_file}}
-        instance.data["ftrackComponents"] = components
