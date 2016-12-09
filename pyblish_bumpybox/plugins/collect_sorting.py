4c5d6f7086bfb7baae8d9a06da8e429f7c6cc116
Toke Jepsen <tokejepsen@gmail.com>
2016-12-09 15:30:19 +0100

    Sorting instances by names

--- pyblish_bumpybox/plugins/collect_sorting.py
+++ pyblish_bumpybox/plugins/collect_sorting.py
@@ -3,10 +3,11 @@ import pyblish.api
 
 class BumpyboxCollectSorting(pyblish.api.Collector):
     # offset to execute last of all collectors
-    order = pyblish.api.Collector.order + 0.4
+    order = pyblish.api.Collector.order + 0.49
     label = "Sorting"
 
     def process(self, context):
 
-        context[:] = sorted(context,
-                            key=lambda instance: (instance.data("label")))
+        context[:] = sorted(
+            context, key=lambda instance: (instance.data["name"])
+        )
