7f48a0a8b4fa212ded82322edce6802021cc1cf8
Toke Jepsen <tokejepsen@gmail.com>
2016-12-14 21:13:04 +0100

    Maya save scene automatic during validation.

--- pyblish_bumpybox/plugins/maya/validate_scene_modified.py
+++ pyblish_bumpybox/plugins/maya/validate_scene_modified.py
@@ -2,27 +2,16 @@ import pymel
 import pyblish.api
 
 
-class BumpyboxMayaRepairSceneModified(pyblish.api.Action):
-
-    label = "Repair"
-    icon = "wrench"
-    on = "failed"
-
-    def process(self, context, plugin):
-
-        pymel.core.saveFile()
-
-
 class BumpyboxMayaValidateSceneModified(pyblish.api.ContextPlugin):
     """ Validates whether the scene has been saved since modifying. """
 
-    order = pyblish.api.ValidatorOrder
+    order = pyblish.api.ValidatorOrder - 0.49
     hosts = ["maya"]
     label = "Scene Modified"
     optional = True
-    actions = [BumpyboxMayaRepairSceneModified]
 
     def process(self, context):
 
-        msg = "Scene has not been saved since modifying."
-        assert not pymel.core.dgmodified(), msg
+        if pymel.core.dgmodified():
+            self.log.info("Scene modified. Saving scene...")
+            pymel.core.saveFile()
