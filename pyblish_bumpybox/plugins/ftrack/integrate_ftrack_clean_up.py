931a4af60d36319c93710a0d09cf6daf03f5b3cf
Toke Jepsen <tokejepsen@gmail.com>
2017-01-27 22:31:50 +0100

    Clean up ftrack integrated workspace files.

---
+++ pyblish_bumpybox/plugins/ftrack/integrate_ftrack_clean_up.py
@@ -0,0 +1,26 @@
+import os
+
+import pyblish.api
+import clique
+
+
+class BumpyboxIntegrateFtrackCleanUp(pyblish.api.InstancePlugin):
+    """ Clean up any workspace files that has been integrated.
+
+    Offset to get component from pyblish-ftrack
+    """
+
+    order = pyblish.api.IntegratorOrder + 0.1
+    label = "Ftrack Clean Up"
+    families = ["ftrack"]
+    optional = True
+
+    def process(self, instance):
+
+        for data in instance.data.get("ftrackComponentsList", []):
+            path = data["component_path"]
+            if "component" in data and "workspace" in path:
+                collection = clique.parse(path)
+                for f in collection:
+                    os.remove(f)
+                    self.log.info("Deleted: \"{0}\"".format(f))
