632daafe2e52dc8e0e5daf4064b4125b0ebe84a7
Toke Jepsen <tokejepsen@gmail.com>
2017-02-02 13:52:07 +0100

    Enabled option to disconnect animation in rigging tasks.

--- pyblish_bumpybox/plugins/maya/extract_maya_formats.py
+++ pyblish_bumpybox/plugins/maya/extract_maya_formats.py
@@ -1,6 +1,7 @@
 import os
 
 import pymel
+import maya.cmds as cmds
 import pyblish.api
 import pyblish_maya
 
@@ -28,8 +29,17 @@ class BumpyboxMayaExtractMayaFormats(pyblish.api.InstancePlugin):
 
         with pyblish_maya.maintained_selection():
 
-            pymel.core.select(instance[0].members(), noExpand=True)
+            # Disconnect animation if requested
+            connections = []
+            if instance.data.get("disconnectAnimation", False):
+                # Disconnect connections to anim curves, and store for later
+                for node in pymel.core.ls(type="transform"):
+                    connections.extend(self.disconnect(node, "animCurveTA"))
+                    connections.extend(self.disconnect(node, "animCurveTU"))
+                    connections.extend(self.disconnect(node, "animCurveTL"))
 
+            # Export selection
+            pymel.core.select(instance[0].members(), noExpand=True)
             export_type = set(self.families) & set(instance.data["families"])
             pymel.core.system.exportSelected(
                 path,
@@ -40,3 +50,33 @@ class BumpyboxMayaExtractMayaFormats(pyblish.api.InstancePlugin):
                     "constructionHistory", True
                 )
             )
+
+            # Reconnect animation
+            for connection in connections:
+                connection["source"] >> connection["destination"]
+
+    def disconnect(self, node, connectionType):
+
+        connections = []
+        for connection in node.connections(type=connectionType, plugs=True):
+            src = connection
+            dst = connection.connections(
+                destination=True, source=False, plugs=True
+            )[0]
+            connections.append({"source": src, "destination": dst})
+            connection // dst
+
+            # Reset attribute
+            values = cmds.attributeQuery(
+                dst.name(includeNode=False),
+                node=node.name(),
+                listDefault=True
+            )
+            try:
+                cmds.setAttr(
+                    node.name() + '.' + dst.name(includeNode=False), *values
+                )
+            except:
+                pass
+
+        return connections
