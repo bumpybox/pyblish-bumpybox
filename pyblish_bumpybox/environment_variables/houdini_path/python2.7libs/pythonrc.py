1b47508f30251e759756f009226e164245693507
Toke Jepsen <tokejepsen@gmail.com>
2017-02-06 16:45:34 +0100

    Code cosmetics

--- pyblish_bumpybox/environment_variables/houdini_path/python2.7libs/pythonrc.py
+++ pyblish_bumpybox/environment_variables/houdini_path/python2.7libs/pythonrc.py
@@ -5,11 +5,12 @@ import pyblish_lite.settings
 # Pyblish callbacks for presisting instance states to the scene
 def toggle_instance(instance, new_value, old_value):
 
-    # all instances are nodes, except for the scene instance
+    # All instances are nodes, except for the scene instance
     try:
         instance[0].bypass(not bool(new_value))
     except:
         pass
+
 
 pyblish.api.register_callback("instanceToggled", toggle_instance)
 
