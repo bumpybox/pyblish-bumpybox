d134815a9aa87875097d7b6bca03bd89cf61c1da
Toke Jepsen <tokejepsen@gmail.com>
2017-02-11 15:34:01 +0100

    Extraction of JSON serialized instances.

---
+++ pyblish_bumpybox/plugins/extract_json.py
@@ -0,0 +1,43 @@
+import os
+import json
+
+import pyblish.api
+import clique
+
+
+class BumpyboxExtractJSON(pyblish.api.ContextPlugin):
+    """ Extract all instances to a serialized json file. """
+
+    order = pyblish.api.IntegratorOrder + 1
+    label = "JSON"
+
+    def process(self, context):
+
+        workspace = os.path.join(
+            os.path.dirname(context.data["currentFile"]), "workspace"
+        )
+
+        if not os.path.exists(workspace):
+            os.makedirs(workspace)
+
+        output_data = []
+        for instance in context:
+
+            data = {}
+            for key, value in instance.data.iteritems():
+                if isinstance(value, clique.Collection):
+                    value = value.format()
+
+                try:
+                    json.dumps(value)
+                    data[key] = value
+                except:
+                    msg = "\"{0}\"".format(value)
+                    msg += " in instance.data[\"{0}\"]".format(key)
+                    msg += " could not be serialized."
+                    self.log.warning(msg)
+
+            output_data.append(data)
+
+        with open(os.path.join(workspace, "instances.json"), "w") as outfile:
+            outfile.write(json.dumps(output_data, indent=4, sort_keys=True))
