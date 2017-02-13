da2aa4641d5369e6846500818ae1b2e3648371ec
Toke Jepsen <tokejepsen@gmail.com>
2017-02-13 13:06:41 +0100

    Change ftrack task status on publish to " In Progress".

---
+++ pyblish_bumpybox/plugins/ftrack/integrate_ftrack_status.py
@@ -0,0 +1,16 @@
+import pyblish.api
+
+
+class BumpyboxIntegrateFtrackStatus(pyblish.api.ContextPlugin):
+    """ Sets the task to "In Progress". """
+
+    order = pyblish.api.IntegratorOrder
+    label = "Ftrack Status"
+    optional = True
+
+    def process(self, context):
+
+        session = context.data["ftrackSession"]
+        status = session.query("Status where name is \"In Progress\"").one()
+        context.data["ftrackTask"]["status"] = status
+        session.commit()
