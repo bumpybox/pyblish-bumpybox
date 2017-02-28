2998e1b6e76e53f366a914a75d1fb56a1795efed
Toke Jepsen <tokejepsen@gmail.com>
2017-02-28 13:40:38 +0100

    Maya: on ftrack launch only change resolution if its the first time launching.

--- pyblish_bumpybox/environment_variables/pythonpath/ftrack_init.py
+++ pyblish_bumpybox/environment_variables/pythonpath/ftrack_init.py
@@ -1,20 +1,34 @@
 import os
 
-import pymel.core
+import pymel.core as pm
 import ftrack
 
 
 def resolutionInit():
-    defaultResolution = pymel.core.PyNode("defaultResolution")
+    defaultResolution = pm.PyNode("defaultResolution")
     task = ftrack.Task(os.environ["FTRACK_TASKID"])
 
-    width = task.getParent().get("width")
-    defaultResolution.width.set(width)
-    pymel.core.warning("Changed resolution width to: {0}".format(width))
-    height = task.getParent().get("height")
-    defaultResolution.height.set(height)
-    pymel.core.warning("Changed resolution height to: {0}".format(height))
+    # Adding/Checking ftrack resolution attribute
+    resolution_set = False
+    if hasattr(defaultResolution, "ftrackResolutionSet"):
+        attr = pm.Attribute("defaultResolution.ftrackResolutionSet")
+        resolution_set = attr.get()
+    else:
+        pm.addAttr(
+            defaultResolution,
+            longName="ftrackResolutionSet",
+            defaultValue=True,
+            attributeType="bool"
+        )
+
+    if not resolution_set:
+        width = task.getParent().get("width")
+        defaultResolution.width.set(width)
+        pm.warning("Changed resolution width to: {0}".format(width))
+        height = task.getParent().get("height")
+        defaultResolution.height.set(height)
+        pm.warning("Changed resolution height to: {0}".format(height))
 
 
 def init():
-    pymel.core.evalDeferred("ftrack_init.resolutionInit()")
+    pm.evalDeferred("ftrack_init.resolutionInit()")
