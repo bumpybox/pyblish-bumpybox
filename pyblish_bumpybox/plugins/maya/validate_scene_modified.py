67fb90f6e43ca28454e49d91c7ba461c57a541b0
Toke Jepsen <tokejepsen@gmail.com>
2017-03-01 11:27:34 +0100

    Maya: delay scene saving to extraction, so you can toggle it off.

--- pyblish_bumpybox/plugins/maya/validate_scene_modified.py
+++ pyblish_bumpybox/plugins/maya/validate_scene_modified.py
@@ -5,7 +5,7 @@ import pyblish.api
 class BumpyboxMayaValidateSceneModified(pyblish.api.ContextPlugin):
     """ Validates whether the scene has been saved since modifying. """
 
-    order = pyblish.api.ValidatorOrder - 0.49
+    order = pyblish.api.ExtractorOrder - 0.49
     hosts = ["maya"]
     label = "Scene Modified"
     optional = True
