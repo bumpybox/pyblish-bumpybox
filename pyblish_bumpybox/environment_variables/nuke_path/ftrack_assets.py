12eb7c79e3a2726ed1384959751cf296f317bdb9
Toke Jepsen <tokejepsen@gmail.com>
2017-03-01 14:10:21 +0100

    Nuke: Ftrack asset scene import settings option.

--- pyblish_bumpybox/environment_variables/nuke_path/ftrack_assets.py
+++ pyblish_bumpybox/environment_variables/nuke_path/ftrack_assets.py
@@ -1,5 +1,8 @@
+import os
+
 import nuke
 
+import ftrack
 from ftrack_connect_nuke.connector.nukeassets import GenericAsset
 from ftrack_connect.connector import FTAssetHandlerInstance, HelpFunctions
 
@@ -13,7 +16,41 @@ class SceneAsset(GenericAsset):
 
         nuke.nodePaste(iAObj.filePath % start)
 
+        if iAObj.options["importSettings"]:
+
+            component = ftrack.Component(iAObj.componentId)
+            parent = component.getVersion().getAsset().getParent()
+
+            # Setup fps
+            nuke.root()['fps'].setValue(parent.get('fps'))
+
+            # Setup frame range
+            nuke.root()['first_frame'].setValue(parent.get('fstart'))
+            nuke.root()['last_frame'].setValue(parent.get('fend'))
+
+            # Setup resolution
+            width = parent.get("width")
+            height = parent.get("height")
+            fmt = None
+            for f in nuke.formats():
+                if f.width() == width and f.height() == height:
+                    fmt = f
+
+            if fmt:
+                nuke.root()['format'].setValue(fmt.name())
+
         return
+
+    @staticmethod
+    def importOptions():
+        xml = """
+        <tab name="Options">
+            <row name="Import Settings" accepts="nuke">
+                <option type="checkbox" name="importSettings" value="False"/>
+            </row>
+        </tab>
+        """
+        return xml
 
 
 def register_assets():
