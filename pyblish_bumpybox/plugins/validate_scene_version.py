bced081ab652c499acc0195937b0b1224be44643
Toke Jepsen <tokejepsen@gmail.com>
2017-06-19 14:59:43 +0200

    Remove redundant plus operator.

--- pyblish_bumpybox/plugins/validate_scene_version.py
+++ pyblish_bumpybox/plugins/validate_scene_version.py
@@ -14,7 +14,7 @@ class ValidateSceneVersion(pyblish.api.ContextPlugin):
 
         name, ext = os.path.splitext(context.data("currentFile"))
         failure_message = (
-            'Could not find a version number in the scene name. Please add ' +
+            'Could not find a version number in the scene name. Please add '
             'v[number] to the scene name, for example: "{0}_v001{1}".'
         ).format(name, ext)
         assert "version" in context.data, failure_message
