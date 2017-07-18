da9355b9d6b79dac4f73cb8f4daa687c3a25af26
Toke Jepsen <tokejepsen@gmail.com>
2017-07-18 11:16:36 +0200

    Remove ftrack_locations Restructure to allow overrides of all component data. Include "scene" family for single outputs.

--- pyblish_bumpybox/plugins/ftrack/extract_ftrack_components.py
+++ pyblish_bumpybox/plugins/ftrack/extract_ftrack_components.py
@@ -1,13 +1,12 @@
 import pyblish.api
-import ftrack_locations
 
 
 class ExtractFtrackComponents(pyblish.api.InstancePlugin):
     """Appending output files from local extraction as components.
 
     This plugin generates component data from either the instance data or
-    defaults. Finding "collection" or "output" in the instance data trigger the
-    component data generation.
+    defaults. Finding "collection" or "output_path" in the instance data
+    trigger the component data generation.
     """
 
     order = pyblish.api.ExtractorOrder + 0.4
@@ -38,7 +37,11 @@ class ExtractFtrackComponents(pyblish.api.InstancePlugin):
 
             # Add component
             families = instance.data.get("families", [])
-            valid_families = ["gizmo", "lut"]
+            valid_families = ["gizmo", "lut", "scene"]
+
+            instance.data["assettype_short"] = list(
+                set(families) & set(valid_families)
+            )[0]
 
             self.add_ftrack_components(
                 instance,
@@ -47,51 +50,42 @@ class ExtractFtrackComponents(pyblish.api.InstancePlugin):
 
     def add_ftrack_components(self, instance, component_path):
 
+        component = {
+            "assettype_data": {
+                "short": instance.data.get("assettype_short", "upload")
+            },
+            "asset_data": {
+                "name": instance.data.get(
+                    "asset_name",
+                    instance.context.data["ftrackTask"]["name"]
+                ),
+                "parent": instance.data.get(
+                    "asset_parent",
+                    instance.context.data["ftrackTask"]["parent"]
+                )
+            },
+            "assetversion_data": {
+                "version": instance.data.get(
+                    "version", instance.context.data["version"]
+                )
+            },
+            "component_data": {
+                "name": instance.data.get(
+                    "component_name", instance.data["name"]
+                ),
+            },
+            "component_path": component_path,
+            "component_overwrite": instance.data.get(
+                "component_overwrite", True
+            ),
+            "component_location": instance.data.get("component_location", None)
+        }
+
+        # Remove component location key if no location was found. This is done
+        # so the session location can be picked up when integrating.
+        if "component_location" not in instance.data:
+            del component["component_location"]
+
         components = instance.data.get("ftrackComponentsList", [])
-        components.append(
-            {
-                "assettype_data": {
-                    "short": instance.data.get("assettype_short", "upload")
-                },
-                "asset_data": {
-                    "name": instance.data.get(
-                        "asset_name",
-                        instance.context.data["ftrackTask"]["name"]
-                    ),
-                    "parent": instance.data.get(
-                        "asset_parent",
-                        instance.context.data["ftrackTask"]["parent"]
-                    )
-                },
-                "assetversion_data": {
-                    "version": instance.data.get(
-                        "version", instance.context.data["version"]
-                    )
-                },
-                "component_data": {
-                    "name": instance.data.get(
-                        "component_name", instance.data["name"]
-                    ),
-                },
-                "component_path": component_path,
-                "component_overwrite": True,
-            }
-        )
+        components.append(component)
         instance.data["ftrackComponentsList"] = components
-
-
-class BumpyboxFtrackExtractLocation(pyblish.api.InstancePlugin):
-    """ Appending output files from local extraction as components. """
-
-    order = ExtractFtrackComponents.order + 0.01
-    label = "Location"
-    families = ["local", "output"]
-
-    def process(self, instance):
-
-        # Setup location
-        session = instance.context.data["ftrackSession"]
-        location = ftrack_locations.get_new_location(session)
-
-        for data in instance.data.get("ftrackComponentsList", []):
-            data["component_location"] = location
