42af9db0a56e4d8ac1dd750f1dc366d0390209a1
Toke Jepsen <tokejepsen@gmail.com>
2017-08-11 10:56:25 +0200

    Manually fix tasks "_projectRoot" attribute.

--- pyblish_bumpybox/plugins/nukestudio/validate_nukestudio_projectroot.py
+++ pyblish_bumpybox/plugins/nukestudio/validate_nukestudio_projectroot.py
@@ -20,6 +20,13 @@ class RepairProjectRoot(pyblish.api.Action):
 
         context.data["activeProject"].setProjectRoot(workspace)
 
+        # Need to manually fix the tasks "_projectRoot" attribute, because
+        # setting the project root is not enough.
+        submission = context.data.get("submission", None)
+        if submission:
+            for task in submission.getLeafTasks():
+                task._projectRoot = workspace
+
 
 class ValidateNukeStudioProjectRoot(pyblish.api.ContextPlugin):
     """Validate the project root to the workspace directory."""
