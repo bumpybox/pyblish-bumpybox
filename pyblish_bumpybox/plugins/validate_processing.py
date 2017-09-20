b52e6c79c9fd513fdd2ae9d780ceae18864fae54
Toke Jepsen <tokejepsen@gmail.com>
2017-09-20 14:31:17 +0200

    Better naming for validate_processing plugin.

--- pyblish_bumpybox/plugins/validate_processing.py
+++ pyblish_bumpybox/plugins/validate_processing.py
@@ -6,21 +6,29 @@ class ValidateProcessing(pyblish.api.ContextPlugin):
 
     order = pyblish.api.ValidatorOrder
     optional = True
-    label = "Instance to Process"
+    label = "Nodes to Process"
     targets = ["process"]
 
     def process(self, context):
 
         instances_to_process = []
+        instance_labels = ""
         for instance in context:
+
             # Ignore source family
             families = instance.data.get("families", [])
             families += [instance.data["family"]]
             if "source" in families:
                 continue
 
+            instance_labels += "\n\n" + instance.data["label"]
+
             if instance.data("publish", True):
                 instances_to_process.append(instance)
 
-        msg = "No instances enabled for processing."
+        msg = (
+            "No nodes were enabled for processing. Please hit reset and choose"
+            " a node to process in the left-hand list.\n\nPossible nodes to "
+            "process:{0}".format(instance_labels)
+        )
         assert instances_to_process, msg
