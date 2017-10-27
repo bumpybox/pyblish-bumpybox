374b50f67849575d4f92c7345426f77bcbfb33ef
Toke Jepsen <tokejepsen@gmail.com>
2017-10-27 17:11:35 +0200

    Make backdrop instances respect selection publishing.

--- pyblish_bumpybox/plugins/nuke/collect_nuke_backdrops.py
+++ pyblish_bumpybox/plugins/nuke/collect_nuke_backdrops.py
@@ -36,7 +36,16 @@ class CollectNukeBackdrops(pyblish.api.ContextPlugin):
                 knob.setValue(False)
                 node.addKnob(knob)
 
-            instance.data["publish"] = bool(node["publish"].getValue())
+            # Compare against selection
+            selection = instance.context.data.get("selection", [])
+            publish = bool(node["publish"].getValue())
+            if selection:
+                if list(set(instance) & set(selection)):
+                    publish = True
+                else:
+                    publish = False
+
+            instance.data["publish"] = publish
 
             # Generate output path
             directory = os.path.join(
