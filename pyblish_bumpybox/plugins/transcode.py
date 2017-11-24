568976d5023145abe219720d555507319b429691
Toke Jepsen <tokejepsen@gmail.com>
2017-11-24 13:58:22 +0100

    Validate existence of djv_convert. Collate executable validators.

--- pyblish_bumpybox/plugins/transcode.py
+++ pyblish_bumpybox/plugins/transcode.py
@@ -5,37 +5,6 @@ import pyblish.api
 import filelink
 
 
-class ValidateTranscode(pyblish.api.InstancePlugin):
-    """Validate environment is ready for extracting a movie."""
-
-    order = pyblish.api.ValidatorOrder
-    label = "Transcode"
-    optional = True
-    families = ["review"]
-
-    def process(self, instance):
-
-        executable = "ffmpeg"
-
-        def is_exe(fpath):
-            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-        fpath, fname = os.path.split(executable)
-        if fpath:
-            if is_exe(executable):
-                return fpath
-        else:
-            for path in os.environ["PATH"].split(os.pathsep):
-                path = path.strip('"')
-                exe_file = os.path.join(path, executable)
-                if is_exe(exe_file):
-                    return exe_file
-                if is_exe(exe_file + ".exe"):
-                    return exe_file + ".exe"
-
-        raise IOError("\"{0}\" executable not found.".format(executable))
-
-
 class ExtractTranscode(pyblish.api.InstancePlugin):
     """Extracts review movie from image sequence.
 
---
+++ pyblish_bumpybox/plugins/validate_executables.py
@@ -0,0 +1,54 @@
+import os
+
+import pyblish.api
+
+
+class ValidateFFmpeg(pyblish.api.ContextPlugin):
+    """Validate FFMPEG is available.
+
+    FFMPEG is being used in various plugins for image and video processing.
+    """
+
+    order = pyblish.api.ValidatorOrder
+    label = "FFMPEG"
+    optional = True
+
+    def get_executable(self, executable):
+
+        def is_exe(fpath):
+            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+        fpath, fname = os.path.split(executable)
+        if fpath:
+            if is_exe(executable):
+                return fpath
+        else:
+            for path in os.environ["PATH"].split(os.pathsep):
+                path = path.strip('"')
+                exe_file = os.path.join(path, executable)
+                if is_exe(exe_file):
+                    return exe_file
+                if is_exe(exe_file + ".exe"):
+                    return exe_file + ".exe"
+
+        raise IOError("\"{0}\" executable not found.".format(executable))
+
+    def process(self, instance):
+
+        self.get_executable("ffmpeg")
+
+
+class ValidateDJV(ValidateFFmpeg):
+    """Validate djv_convert is available.
+
+    djv_convert is being used in various plugins for image and video
+    processing.
+    """
+
+    order = pyblish.api.ValidatorOrder
+    label = "DJV"
+    optional = True
+
+    def process(self, instance):
+
+        self.get_executable("djv_convert")
