9952a2ed0b9c8a0e1013205e8673401cbc649b0b
Toke Jepsen <tokejepsen@gmail.com>
2017-12-20 15:08:56 +0100

    Link source version to assetversions in Ftrack.

---
+++ pyblish_bumpybox/plugins/ftrack/other_ftrack_link_source.py
@@ -0,0 +1,49 @@
+from pyblish import api
+
+
+class OtherFtrackLinkSource(api.ContextPlugin):
+    """Link the source version to all other versions."""
+
+    order = api.IntegratorOrder + 1
+    label = "Ftrack Link Source"
+
+    def process(self, context):
+
+        # Collect source assetversion
+        source_version = None
+        for instance in context:
+            # Filter to source instance
+            families = [instance.data["family"]] + instance.data["families"]
+            if "source" not in families:
+                continue
+
+            # Get AssetVersion from published component
+            for data in instance.data["ftrackComponentsList"]:
+                if "component" not in data.keys():
+                    continue
+                source_version = data["component"]["version"]
+
+        # Skip if no source version were published
+        if source_version is None:
+            return
+
+        for instance in context:
+            # Filter to non source instance
+            families = [instance.data["family"]] + instance.data["families"]
+            if "source" in families:
+                continue
+
+            # Get AssetVersion from published component
+            for data in instance.data.get("ftrackComponentsList", []):
+                if "component" not in data.keys():
+                    continue
+
+                context.data["ftrackSession"].create(
+                    "AssetVersionLink",
+                    {
+                        "from": source_version,
+                        "to": data["component"]["version"]
+                    }
+                )
+
+        context.data["ftrackSession"].commit()
