054223d8bb365899b9eb2765891d4a26d5b7245f
Toke Jepsen <tokejepsen@gmail.com>
2018-01-25 17:45:00 +0100

    Code cosmetics.

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,8 @@ import sys
 import pytest
 
 
-sys.exit(pytest.main(
-    [os.path.abspath(os.path.join(os.path.dirname(__file__), "tests"))]
-))
+sys.exit(
+    pytest.main(
+        [os.path.abspath(os.path.join(os.path.dirname(__file__), "tests"))]
+    )
+)
