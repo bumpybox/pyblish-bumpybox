9bf5ecad344e7d4365a0afc47cf7551aefa4b49f
Toke Jepsen <tokejepsen@gmail.com>
2018-02-20 12:43:53 +0100

    Making all Maya Deadline settings optional.

--- pyblish_bumpybox/plugins/deadline/extract_maya.py
+++ pyblish_bumpybox/plugins/deadline/extract_maya.py
@@ -20,20 +20,16 @@ class ExtractMaya(api.InstancePlugin):
 
         collection = instance.data["collection"]
 
-        data = instance.data.get(
-            "deadlineData", {"job": {}, "plugin": {}}
-        )
-
-        # Setting job data.
-        data["job"]["Plugin"] = "MayaBatch"
-
-        fmt = "{head}" + "#" * collection.padding + "{tail}"
-        data["job"]["OutputFilename0"] = collection.format(fmt)
-
-        data["job"]["Priority"] = instance.data["deadlinePriority"]
-        data["job"]["Pool"] = instance.data["deadlinePool"]
-        value = instance.data["deadlineConcurrentTasks"]
-        data["job"]["ConcurrentTasks"] = value
+        # Getting job data.
+        job_data = {
+            "Plugin": "MayaBatch",
+            "OutputFilename0": collection.format(
+                "{head}" + "#" * collection.padding + "{tail}"
+            ),
+            "Priority": instance.data["deadlinePriority"],
+            "Pool": instance.data["deadlinePool"],
+            "ConcurrentTasks": instance.data["deadlineConcurrentTasks"]
+        }
 
         # Frame range
         render_globals = pymel.core.PyNode("defaultRenderGlobals")
@@ -41,41 +37,57 @@ class ExtractMaya(api.InstancePlugin):
         end_frame = int(render_globals.endFrame.get())
         step_frame = int(render_globals.byFrameStep.get())
 
-        data["job"]["Frames"] = "{0}-{1}x{2}".format(
+        job_data["Frames"] = "{0}-{1}x{2}".format(
             start_frame, end_frame, step_frame
         )
 
         # Chunk size
-        data["job"]["ChunkSize"] = instance.data["deadlineChunkSize"]
+        job_data["ChunkSize"] = instance.data["deadlineChunkSize"]
         if len(list(collection)) == 1:
-            data["job"]["ChunkSize"] = str(end_frame)
+            job_data["ChunkSize"] = str(end_frame)
         else:
             tasks = (end_frame - start_frame + 1.0) / step_frame
-            chunks = (end_frame - start_frame + 1.0) / data["job"]["ChunkSize"]
+            chunks = (end_frame - start_frame + 1.0) / job_data["ChunkSize"]
             # Deadline can only handle 5000 tasks maximum
             if tasks > 5000 and chunks > 5000:
-                data["job"]["ChunkSize"] = str(int(math.ceil(tasks / 5000.0)))
+                job_data["ChunkSize"] = str(int(math.ceil(tasks / 5000.0)))
 
         # Setting plugin data
-        data["plugin"]["Renderer"] = "file"
-        data["plugin"]["UseLegacyRenderLayers"] = 0
-        data["plugin"]["UsingRenderLayers"] = 1
-        data["plugin"]["RenderLayer"] = instance[0].name()
-        data["plugin"]["Version"] = versions.flavor()
-        data["plugin"]["MaxProcessors"] = 0
-
-        scene_file = instance.context.data["currentFile"]
-        data["plugin"]["SceneFile"] = scene_file
-        data["plugin"]["ProjectPath"] = os.path.dirname(scene_file)
-        data["plugin"]["OutputFilePath"] = os.path.join(
-            os.path.dirname(scene_file), "workspace"
-        )
+        plugin_data = {
+            "Renderer": "file",
+            "UseLegacyRenderLayers": 0,
+            "UsingRenderLayers": 1,
+            "RenderLayer": instance[0].name(),
+            "Version": versions.flavor(),
+            "MaxProcessors": 0,
+            "LocalRendering": 1,
+            "SceneFile": instance.context.data["currentFile"],
+            "ProjectPath": os.path.dirname(
+                instance.context.data["currentFile"]
+            ),
+            "OutputFilePath": os.path.join(
+                os.path.dirname(instance.context.data["currentFile"]),
+                "workspace"
+            )
+        }
 
         # Arnold plugin settings
         if "arnold" in instance.data.get("families", []):
-            data["plugin"]["Renderer"] = "arnold"
-            data["plugin"]["ArnoldVerbose"] = 1
-            data["plugin"]["Animation"] = 1
+            plugin_data["Renderer"] = "arnold"
+            plugin_data["ArnoldVerbose"] = 1
+            plugin_data["Animation"] = 1
 
         # Setting data
+        data = instance.data.get(
+            "deadlineData", {"job": {}, "plugin": {}}
+        )
+
+        for key, value in job_data.iteritems():
+            if key not in data["job"].keys():
+                data["job"][key] = value
+
+        for key, value in plugin_data.iteritems():
+            if key not in data["plugin"].keys():
+                data["plugin"][key] = value
+
         instance.data["deadlineData"] = data
